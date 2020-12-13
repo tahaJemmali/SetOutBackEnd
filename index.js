75c239ec319f820ca171e708b892d98bce3d5653
tahaJemmali <49129221+tahaJemmali@users.noreply.github.com>
2020-12-13 12:48:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,35 +23,53 @@ mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true},function (e
        console.log('Unable to connect to the mongodb server.Error', err);
    }else{
 
-       //////////////routing 
-        //tasks
-        app.get('/all_tasks',(req,res) =>{taskRoutes.getAllTasksRoute(res)})
-        app.post('/add_task',(req,res) =>{taskRoutes.addTaskRoute(req,res)})
-        app.delete('/delete_task/:id',(req,res) =>{taskRoutes.deleteTaskRoute(req.params.id,res)})
-        app.put('/update_task/:id',(req,res) =>{var reqBody=req.body;var id =req.params.id;taskRoutes.updateTaskRoute(id,reqBody,res)})
-        //schedule
-        app.get('/all_schedules',(req,res) =>{taskRoutes.getAllSchedules(res)})
-
-        //project
-        app.post('/add_project',(req,res) =>{projectRoutes.addprojectRoute(req,res)})
-
-
-       //Register | Sing Up
-        app.post('/register/:firstName/:lastName/:email/:password/:address/:phone',(request,response)=>{userRoutes.Register(request,response)});
-
-       //Login
-       app.post('/login/:email/:password',(request,response)=>{userRoutes.Login(request,response)});
-
-       //GetUser
-       app.get('/getUser/:email',(request,response)=>{/*request._custom = request.get('email');*/var email=request.params.email;userRoutes.GetUser(email,request,response)});
-
-       //updateUserEmail
-       app.put('/updateUserEmail/:oldEmail/:newEmail/:password',(request,response)=>{userRoutes.UpdateUserEmail(request,response)});
-
-       //LoginWithFacebook
-       app.post('/LoginWithFacebook/:firstName/:lastName/:email/:signedUpWith/:birthDate/:photo',(request,response)=>{userRoutes.LoginWithFacebook(request,response)});
-
-
+    app.get('/',(req,res) =>{res.send("Welcome to set-out")})
+    //tasks
+    app.get('/all_tasks',(req,res) =>{taskRoutes.getAllTasksRoute(res)})
+    app.post('/add_task',(req,res) =>{taskRoutes.addTaskRoute(req,res)})
+    app.delete('/delete_task/:id',(req,res) =>{taskRoutes.deleteTaskRoute(req.params.id,res)})
+    app.put('/update_task/:id',(req,res) =>{var reqBody=req.body;var id =req.params.id;taskRoutes.updateTaskRoute(id,reqBody,res)})
+    //schedule
+    app.get('/all_schedules',(req,res) =>{taskRoutes.getAllSchedules(res)})
+    
+    //balance
+    app.post('/add_balance',(req,res) =>{balanceRoutes.addbalanceRoute(req,res)})
+    app.get('/all_balances',(req,res) =>{balanceRoutes.getAllbalancesRoute(res)})
+    app.delete('/delete_balance/:id',(req,res) =>{balanceRoutes.deletebalanceRoute(req.params.id,res)})
+  
+    //project
+    app.post('/add_project',(req,res) =>{projectRoutes.addprojectRoute(req,res)})
+    app.get('/all_projects',(req,res) =>{projectRoutes.getAllprojectsRoute(res)})
+    app.delete('/delete_project/:id',(req,res) =>{projectRoutes.deleteprojectRoute(req.params.id,res)})
+  
+   //tags
+   app.post('/add_tag',(req,res) =>{tagRoutes.addTagRoute(req,res)})
+   app.get('/all_tags',(req,res) =>{tagRoutes.getAllTagsRoute(res)})
+  
+  
+  
+   //Register | Sing Up
+    app.post('/register',(request,response)=>{userRoutes.Register(request,response)})
+  
+   //Login
+   app.post('/login',(request,response)=>{userRoutes.Login(request,response)})
+  
+   //GetUser
+   app.get('/getUser/:email',(request,response)=>{var email=request.params.email;userRoutes.GetUser(email,request,response)})
+  
+   //passwordRecovery
+   app.post('/passwordRecovery',(request,response)=>{userRoutes.GetRecoveryCode(request,response)})
+  
+   //passwordReset
+   app.put('/passwordReset',(request,response)=>{userRoutes.ResetPassword(request,response)})
+  
+  
+   //updateUserEmail
+   app.put('/updateUserEmail',(request,response)=>{userRoutes.UpdateUserEmail(request,response)})
+  
+   //LoginWithFacebook
+   app.post('/LoginWithFacebook',(request,response)=>{userRoutes.LoginWithFacebook(request,response)})
+  
        //Start Web Server
        app.listen(PORT,()=>{
            console.log('connected to mongodb server, Webserver running on port '+PORT)
